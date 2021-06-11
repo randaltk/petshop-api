9f815bda5fbf4889e15041f2eac5c4ef1e6350ca
randaltk <randal.krebs@qrvconsultoria.com.br>
2021-06-11 13:22:49 -0300

    mongoDb criando produto

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
@@ -10,7 +11,16 @@ app.use(
   })
 );
 
-// Carrega as rotas
+// Conecta no banco
+mongoose.connect(
+  "mongodb+srv://randaltk:dorotitk1@cluster0.avwfj.mongodb.net/petshop-api?retryWrites=true&w=majority"
+);
+
+// Carrega os Models
+
+const Product = require("./models/product");
+
+// Carrega as Rotas
 const indexRoute = require("./routes/index-route");
 const productRoute = require("./routes/product-route");
 
