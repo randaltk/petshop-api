137242368f5ee8bcb4e23b83049ae0cb5f61f57d
randaltk <randal.krebs@qrvconsultoria.com.br>
2021-06-11 12:30:56 -0300

    controllers

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@
 
 const express = require("express");
 const app = express();
-const router = express.Router();
 
 app.use(express.json());
 app.use(
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -1,23 +1,14 @@
 "use strict";
 
 const express = require("express");
-
 const router = express.Router();
 
-router.post("/", (req, res, next) => {
-  res.status(201).send(req.body);
-});
+const controller = require("../controllers/product-controller");
 
-router.put("/:id", (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).send({
-    id: id,
-    item: req.body,
-  });
-});
+router.post("/", controller.post);
 
-router.delete("/", (req, res, next) => {
-  res.status(200).send(req.body);
-});
+router.put("/:id", controller.put);
+
+router.delete("/", controller.delete);
 
 module.exports = router;
